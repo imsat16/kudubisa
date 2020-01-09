44d3dae865305a911001907b672352d6c63bcd84
zamzamkamal <zamzamkamal31@gmail.com>
2020-01-09 14:52:50 +0700

    menambahkan pesan baru di js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 console.log('ok');
 function pesan () {
+	alert("menambahkan pesan dengan eksternal javascript");
 	alert("menghapus pesan dengan eksternal javascript");
+	alert("mengganti pesan dengan eksternal javascript");
 }
